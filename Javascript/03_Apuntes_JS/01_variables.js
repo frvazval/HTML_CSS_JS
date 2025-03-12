fdc869c96079121c16487ed22c3b7a4438858156
Fvazquez <foapgenerico@365.stucom.com>
2025-03-12 17:13:40 +0100

    stucom

--- Javascript/03_Apuntes_JS/01_variables.js
+++ Javascript/03_Apuntes_JS/01_variables.js
@@ -5,3 +5,7 @@ var num1 = 5 // Con var si se puede hacer, porque la reasigna
 
 let num2 = 2 // No se puede volver a utilizar let num2 = 4 porque la variable ya esta creada
 num2 = 3
+
+let num3
+console.log(typeof num3);
+
