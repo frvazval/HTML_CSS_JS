ad3cfc311cfe9ebf4e376aff5104292f17e9bf54
Fvazquez <foapgenerico@365.stucom.com>
2025-03-12 17:29:31 +0100

    stucom

--- Javascript/03_Apuntes_JS/02_condicionales.js
+++ Javascript/03_Apuntes_JS/02_condicionales.js
@@ -3,5 +3,22 @@
 // Python: if - elif - else
 // JS: if - else if -else
 
-const edad = 20
+const edad = 17
 
+if (edad >= 18) {
+    console.log("Eres mayor de edad");
+} else if (edad == 17) {
+    console.log("Ya te falta poco");
+} else {
+    console.log("Aun no puedes votar");
+}
+
+let curso = "Javascript"
+let prueba = true
+
+if (prueba) {
+    let curso = "PHP"
+    console.log(curso);
+}
+
+console.log(curso);
