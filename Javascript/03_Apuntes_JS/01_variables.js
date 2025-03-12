d8b4045078dac5f88fbd38d2cc9c8d904cce34c4
Fvazquez <foapgenerico@365.stucom.com>
2025-03-12 17:20:40 +0100

    stucom

--- Javascript/03_Apuntes_JS/01_variables.js
+++ Javascript/03_Apuntes_JS/01_variables.js
@@ -8,4 +8,11 @@ num2 = 3
 
 let num3
 console.log(typeof num3);
+num3 = 2.5
+console.log(typeof num3);
+
+// Destructuring
+let [txt1, txt2, txt3] = ["a", "b", "c"]
+console.log(txt1, txt2, txt3);
+console.log(typeof txt1);
 
---
+++ Javascript/03_Apuntes_JS/02_condicionales.js
@@ -0,0 +1,7 @@
+// CONDICIONALES
+
+// Python: if - elif - else
+// JS: if - else if -else
+
+const edad = 20
+
