15341f987bb1d6f8a0f3d9c48b1b3fec42b38aa3
Fvazquez <foapgenerico@365.stucom.com>
2025-03-12 20:01:01 +0100

    stucom

--- Javascript/03_Apuntes_JS/05_strings.js
+++ Javascript/03_Apuntes_JS/05_strings.js
@@ -47,4 +47,31 @@ console.log(frase.endsWith("er")); // false
 console.log(palabra.slice(1)); // le quita el primer caracter
 console.log(palabra.slice(1, 4)); // muestra desde el 1 hasta el 3
 
+// split(elemento separador) --> es un array (lista)
+console.log(frase.split(" ")); // Array(8) [ 'Como', 'no', 'estudies', 'Javascript', 'te', 'vas', 'a', 'enterar' ]
+
+let lista = [] // array vacio
+let string = "" // string vacio
+
+let nombre = "mAriA de LaS merceDes y dE TOdaS lAs SantAS"
+
+// Resultado: Maria de las Mercedes y de Todas las Santas
+
+// paso 1: convertir todo a minusculas
+nombre = nombre.toLocaleLowerCase()
+
+// paso 2: obtener las palabras --> split(" ")
+nombre = nombre.split(" ")
+let nombreCorregido = ""
+for (palabra of nombre) {
+    if (palabra != "de"  && palabra != "las" && palabra != "y" && palabra != "los") {
+        let inicial  = palabra.at(0).toUpperCase() // primera letra
+        let restoNombre = palabra.slice(1) // el resto de la palabra hasta el final
+
+        palabra = inicial + restoNombre
+    }
+
+    nombreCorregido += palabra + " "
+}
+console.log(nombreCorregido);
 
