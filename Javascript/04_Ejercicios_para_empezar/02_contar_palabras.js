ff525a90750e7021cccd98334a7f91058f8b5a19
Fvazquez <foapgenerico@365.stucom.com>
2025-03-13 17:05:06 +0100

    stucom

---
+++ Javascript/04_Ejercicios_para_empezar/02_contar_palabras.js
@@ -0,0 +1,19 @@
+// Vamos a tener una frase. Por ejemplo:
+// Mañana será viernes y aunque va a llover será un día maravilloso
+// Hay que mostrar cuantas palabras contienen una letra determinada
+
+const frase = "Mañana será viernes y aunque va a llover será un día maravilloso"
+const letra = "l"
+let contador = 0
+let palabras = frase.split(" ")
+
+for (palabra of palabras) {
+    if (palabras.indexOf(palabra) != 0) {
+        contador++
+    }
+
+}
+
+if (contador == 0) console.log(`La letra ${letra} no aparece en ninguna palabra de la frase`);
+else if (contador == 1) console.log(`La letra ${letra} aparece en 1 palabra de la frase`);
+else console.log(`La letra ${letra} aparece en ${contador} palabras de la frase`);
