6526b66593c4ba5644f638a45956aa0e601ee0be
Fvazquez <foapgenerico@365.stucom.com>
2025-03-13 17:47:56 +0100

    stucom

--- Javascript/03_Apuntes_JS/07_objetos.js
+++ Javascript/03_Apuntes_JS/07_objetos.js
@@ -8,6 +8,7 @@ alumno["apellido"] = "Pi"
 alumno["edad"] = 25
 
 let alumno2 = {nombre: "Pep", apellido: "Guardiola", edad: 55} // La clave tambien se puede poner entre comillas "nombre" : "Pep"
+alumno2.cp = "08906" // Le añade cp y el valor al objeto
 
 let clase = {
     alumnos : [alumno, alumno2]
@@ -18,5 +19,5 @@ let clase = {
 for (claves in alumno) {    
     console.log(claves); // devuelve 'nombre', 'apellido', 'edad'
     console.log(claves, alumno[claves]); // devuelve ['nombre', 'Filemon'], ['apellido', 'Pi'], ['edad', 25]
-
+    console.log(alumno2.cp); // Devuelve '08906'
 }
