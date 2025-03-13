0a6d14f1f783b10139599b0e2501d6ab0d7564fa
Fvazquez <foapgenerico@365.stucom.com>
2025-03-13 19:30:28 +0100

    stucom

--- Javascript/04_Ejercicios_para_empezar/03_devolver_cambio.js
+++ Javascript/04_Ejercicios_para_empezar/03_devolver_cambio.js
@@ -5,15 +5,23 @@
 
 const moneda = [500, 200, 100, 50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01]
 
-let cambio = 427.28
+let cambio = 457.28
+let parteEntera = parseInt(cambio)
+
+let mensaje = ""
 
 let cantidadMonedas = {} // 200: cantidad, solo los que tengan alguna cantidad
 
 
 for (tipo of moneda) {
-    
+    if (parseInt(parteEntera / tipo) != 0) {
+        cantidadMonedas[tipo] = parseInt(parteEntera / tipo)
+        mensaje += parseInt(parteEntera / tipo) + " billetes de " + tipo + " "
+        parteEntera = parteEntera % tipo
+        
+    } 
 } 
 
 console.log(cantidadMonedas);
-
+console.log(mensaje);
 
