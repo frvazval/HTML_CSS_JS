3e3ad32df49b587d23d034f183e0bc92b5415016
Fvazquez <foapgenerico@365.stucom.com>
2025-03-13 15:25:05 +0100

    stucom

---
+++ Javascript/03_Apuntes_JS/06_arrays.js
@@ -0,0 +1,10 @@
+// ARRAYS
+
+let frutas = ["kiwi", "pera", "fresa", "pomelo"]
+
+// El tamaño de un array. En Python era len(frutas)
+// en JS frutas.length
+
+let tamaño_array = frutas.length
+console.log(tamaño_array);
+
