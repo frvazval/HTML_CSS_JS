7360bf1cda38e38e80fafda950c4c815c76487fb
Fvazquez <foapgenerico@365.stucom.com>
2025-03-13 18:00:44 +0100

    stucom

---
+++ Javascript/04_Ejercicios_para_empezar/03_devolver_cambio.js
@@ -0,0 +1,10 @@
+// Vamos a trabajar para un comercio que vende toda clase de articulos,
+// desde los más caros a los más baratos. El cambio se va a devolver en metálico
+// según estas condicion:
+// --- Sera usando la mínima cantidad de billetes y monedas posible
+
+const tipoMoneda = [0.01, 0.02, 0.05, 0.10, 0.20, 0.50, 1, 2, 5, 10, 20, 50, 100, 200, 500]
+const cambio = 427.28
+
+
+
