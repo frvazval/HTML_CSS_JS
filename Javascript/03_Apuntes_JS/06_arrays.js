e98c71e3ee8543dabb0f8b5e55b5777f52e62d93
Fvazquez <foapgenerico@365.stucom.com>
2025-03-13 15:36:29 +0100

    stucom

--- Javascript/03_Apuntes_JS/06_arrays.js
+++ Javascript/03_Apuntes_JS/06_arrays.js
@@ -1,10 +1,34 @@
 // ARRAYS
 
-let frutas = ["kiwi", "pera", "fresa", "pomelo"]
+let frutas = ["kiwi", "pera", "fresa", "pomelo"] // El primero tiene el indice 0
+
+console.log(frutas);
+
+frutas[3] = "limón"
+
+console.log(frutas);
 
 // El tamaño de un array. En Python era len(frutas)
 // en JS frutas.length
 
 let tamaño_array = frutas.length
 console.log(tamaño_array);
+
+// Cómo añadir un elemento al final: push(elemento a añadir)
+frutas.push("mandarina")
+console.log(frutas);
+
+// Cómo eliminar el ultimo elemento: pop(), si quieres lo puedes guardar en una variable
+let frutaEliminada = frutas.pop()
+console.log(frutaEliminada);
+console.log(frutas);
+
+// Cómo añadir un elemento al principio: unshift(elemento a añadir)
+frutas.unshift(frutaEliminada)
+console.log(frutas);
+
+// Cómo eliminar el elemento inicial: shift(), tambien se puede guardar en una variable
+frutaEliminada = frutas.shift()
+console.log(frutaEliminada);
+console.log(frutas);
 
