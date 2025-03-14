e36eb4c850ba9b50c0a5c6997cd93a0e26dde2b6
Fvazquez <foapgenerico@365.stucom.com>
2025-03-14 15:41:06 +0100

    stucom

--- Javascript/03_Apuntes_JS/08_funciones.js
+++ Javascript/03_Apuntes_JS/08_funciones.js
@@ -1,10 +1,12 @@
 // FUNCIONES
 
-// Declaración
+// LLamada o invocación, en Javascript las puedes llamar antes de declararlas
+console.log(sumar(1,2));
+
+// Declaración, se puede hacer al final del código, tambien se pueden poner en un fichero aparte y serian librerias
 function sumar(num1, num2) {
     return num1 + num2
 }
 
-// LLamada o invocación
-console.log(sumar(1,2));
+
 
