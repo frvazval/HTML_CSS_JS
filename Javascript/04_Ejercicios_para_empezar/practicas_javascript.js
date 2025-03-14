b413275666f14770b9aa4b4bee7c21c49ccafd19
Fvazquez <foapgenerico@365.stucom.com>
2025-03-14 19:44:26 +0100

    stucom

---
+++ Javascript/04_Ejercicios_para_empezar/practicas_javascript.js
@@ -0,0 +1,66 @@
+// Se te proponen una serie de ejercicios para practicar
+// los arrays y el bucle for.
+
+// Para resolver NO hay que utilizar ninguna funciÃ³n matemÃ¡tica
+// Crea el cÃ³digo necesario para resolver los requerimientos
+
+// Dado este array:
+let arrayNumeros1 = [4, 5, 3, 8, 2, 7, 1, 6]
+let arrayNumeros2 = [4, 2, 7, 1, 6]
+// o cualquier otro array solo con nÃºmeros
+
+// 1) Mostrar por consola la suma de todos los valores
+
+
+// 2) Mostrar por consola el promedio
+
+
+// 3) Encontrar los valores mÃ¡ximo y mÃ­nimo
+
+
+// 4) Sumar los valores con Ã­ndice par y restar los impares
+
+
+// Hay que mostrar por consola cada resultado
+
+// ====================================================================================================
+
+// Para este array:
+let arrayNombres2 = ["Clint", "Robert", "James", "Anne", "Ingrid", "John", "Patricia", "Marie"]
+
+// 5) Programa el cÃ³digo para encontrar el elemento con el texto mÃ¡s largo
+// y guardarlo en la variable varTextoMasLargo
+// Si hay mÃ¡s de un valor, guardarlos en el array arrayTextosMasLargos.
+
+
+// 6) Lo mismo para el texto mÃ¡s corto.
+
+
+// 7) ObtÃ©n un array llamado longitudNombres que tenga como elementos las longitudes de los textos
+// incluidos en cualquiera de los arrays anteriores. Por tanto debes mostrar : [ 5, 6, 5, etc.
+
+
+// 8) Crea un array llamado arrayNombresConI que incluya solo los nombres que contengan la letra i
+
+// ====================================================================================================
+
+// Dado este array:
+let arrayMixto = [ "Marie", 24, "Pol", 18, "Judith", 22, "Eva", 28 ]
+
+// 9) Debes obtener otro array llamado arrayBidimensional que sea asÃ­:
+// [ ["Marie", 24 ], ["Pol", 18], ["Judith", 22 ], [ "Eva", 28] ]
+
+// ====================================================================================================
+
+// 10) Este array incluye una operaciÃ³n de compra:
+const compra = [
+    ["Leche", 1.2, 2],
+    ["Pan", 0.8, 3],
+    ["Huevos", 2.5, 1],
+    ["CafÃ©", 5.2, 0.5]
+]
+// Se muestra el nombre del artÃ­culo, su precio y la cantidad comprada.
+// Debes obtener la cantidad de artÃ­culos comprados (no de cada tipo) y el importe total.
+// Por ejemplo: "Has comprado 4 artÃ­culos y el importe total es de 12.7 euros"
+
+// DespuÃ©s aÃ±ade otro articulo al array anterior y muestra de nuevo el mensaje informativo con los nuevos datos.
