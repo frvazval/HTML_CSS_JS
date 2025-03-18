2c9351d2087a18f138fce4cbd5415f0243d8bbcc
Fvazquez <foapgenerico@365.stucom.com>
2025-03-18 16:36:54 +0100

    stucom

--- 05_ejemplos_eventos/js/script.js
+++ 05_ejemplos_eventos/js/script.js
@@ -3,6 +3,7 @@ const cabriolet = document.getElementById('cabriolet')
 //  const cabriolet = document.querySelector('#cabriolet')
 
 const alquiler = document.querySelector('#alquiler')
+const importe = document.querySelector('main section:nth-child(2)')
 
 
 cabriolet.addEventListener("click", () => {
@@ -10,4 +11,8 @@ cabriolet.addEventListener("click", () => {
     let precio = cabriolet.querySelector('ul li:last-child').textContent
     precio = precio.split('€')[0]
     alert(precio)
+    importe.style.display = "block"
+    let alquilerTotal = parseFloat(precio) * parseInt(dias)
+    let mensajeUsuario = `${dias} días de alquiler x ${precio}€/día = ${alquilerTotal}€`
+    alquiler.textContent = mensajeUsuario
 })
