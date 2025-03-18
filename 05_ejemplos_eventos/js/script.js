a71edbd232056cffb36ded232756fc07a180ae55
Fvazquez <foapgenerico@365.stucom.com>
2025-03-18 16:29:28 +0100

    stucom

--- 05_ejemplos_eventos/js/script.js
+++ 05_ejemplos_eventos/js/script.js
@@ -1,3 +1,13 @@
 // Obtener el nodo con id cabriolet
 const cabriolet = document.getElementById('cabriolet')
 //  const cabriolet = document.querySelector('#cabriolet')
+
+const alquiler = document.querySelector('#alquiler')
+
+
+cabriolet.addEventListener("click", () => {
+    let dias = prompt("¿Días de alquiler?")
+    let precio = cabriolet.querySelector('ul li:last-child').textContent
+    precio = precio.split('€')[0]
+    alert(precio)
+})
