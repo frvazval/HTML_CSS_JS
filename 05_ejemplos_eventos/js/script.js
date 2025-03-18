9f361d65543ee1a9c5b2b080cc3571857ce27c51
Fvazquez <foapgenerico@365.stucom.com>
2025-03-18 17:15:15 +0100

    stucom

--- 05_ejemplos_eventos/js/script.js
+++ 05_ejemplos_eventos/js/script.js
@@ -9,11 +9,18 @@ const importe = document.querySelector('main section:nth-child(2)')
 cabriolet.addEventListener("click", () => {
     let dias = prompt("¿Días de alquiler?")
     let precio = cabriolet.querySelector('ul li:last-child').textContent
-    precio = precio.split('€')[0]
-    alert(precio)
+    precio = precio.split('€')[0]   
     importe.style.display = "block"
     let alquilerTotal = parseFloat(precio) * parseInt(dias)
     let mensajeUsuario = `${dias} días de alquiler x ${precio}€/día = ${alquilerTotal.toFixed(2)}€`
-    alquiler.textContent = mensajeUsuario
+    alquiler.innerHTML = mensajeUsuario
     
 })
+
+function alquilarVehiculo(precioDia) {
+    let dias = prompt("¿Días de alquiler?")        
+    importe.style.display = "block"
+    let alquilerTotal = parseFloat(precioDia) * parseInt(dias)
+    let mensajeUsuario = `${dias} días de alquiler x ${precioDia}€/día = ${alquilerTotal.toFixed(2)}€`
+    alquiler.innerHTML = mensajeUsuario
+}
