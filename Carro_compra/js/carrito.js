37b581d48addf5d766bdb035e6dc05625997626a
Fvazquez <fvazquez2@hotmail.com>
2025-03-20 13:06:48 +0100

    casa 20/03/2025

---
+++ Carro_compra/js/carrito.js
@@ -0,0 +1,27 @@
+/*
+Hay que programar un carrito de compra de fruta.
+
+El cliente eligirá que fruta quiere haciendo click sobre la imagen.
+Un mensaje emergente le preguntará qué cantidad quiere.
+
+Esta información se mostrará a la derecha, bajo "Total carrito", 
+en <p id="carrito"></p>, de esta forma:
+ Kiwi 2 kg x 4,20€/kg = 8,40 €
+
+El total se actualizará con cada compra
+ Total Compra: 8,40€
+ 
+Se dará la opción de añadir o no más productos que se mostrarán
+a continuación de los anteriores, y se sumará todo en el total. 
+Por ejemplo:  
+ Kiwi 2 kg x 4, 20€/kg = 8, 40€
+ Pomelo 1 kg x 2,50€/kg = 2,50€
+ Total Compra: 10,90€
+
+Puedes modificar el código facilitado si ello te ayuda con el ejercicio,
+pero deberás justificarlo.
+
+Recuerda la importancia comentar con detalle el código.
+
+ Lo importante es el cálculo, no los estilos css
+ */
