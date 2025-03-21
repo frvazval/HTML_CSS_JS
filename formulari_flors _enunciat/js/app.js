d453bf45f74df7f12a0ad7ad2bf779fe36cc8aaa
Fvazquez <foapgenerico@365.stucom.com>
2025-03-21 15:26:47 +0100

    stucom

---
+++ formulari_flors _enunciat/js/app.js
@@ -0,0 +1,95 @@
+// Datos de trabajo
+
+const flores = [
+  { nombre: "rosa", color: "rojo", floracion: "primavera", stock: true },
+  { nombre: "rosa", color: "blanco", floracion: "verano", stock: true },
+  { nombre: "jazmín", color: "blanco", floracion: "verano", stock: false },
+  { nombre: "crisantemo", color: "blanco", floracion: "otoño", stock: false },
+  { nombre: "cerezo", color: "blanco", floracion: "primavera", stock: false },
+  { nombre: "clavel", color: "rojo", floracion: "verano", stock: true },
+];
+
+flores.sort((a, b) => {
+  return a.nombre.localeCompare(b.nombre, "es-ES", { numeric: true });
+});
+
+// ==============================================================================
+// EJERCICIO 1
+
+// console.log(flores);
+
+// Tiene que mostrarse en el HTML los datos de las flores como lista
+// Flor: rosa, de color rojo, florece en primavera y tenemos stock
+// Se mostrará el resultado en #ejercicio1
+
+// for clásico
+// for (let i = 0; i < flores.length; i++) {
+//     console.log(flores[i]);
+// }
+
+
+
+
+// ==============================================================================
+// EJERCICIO 2
+// Listar las flores de color blanco que florecen en verano
+// Modelo de mensaje de salida:
+// Flor: rosa, de color blanco, florece en verano y tenemos stock
+// Se mostrará el resultado en #ejercicio2
+
+
+
+// ==============================================================================
+// EJERCICIO 3
+
+// A partir del formulario incluido, hay que mostrar que datos
+// corresponden a la selección realizada.
+// Se mostrarán en forma de lista como los modelos anteriores.
+// Si no hay ninguna flor que cumpla las condiciones, se mostrará este mensaje:
+// "No hay flor que cumpla las condiciones"
+// Se mostrará el resultado en #ejercicio3
+
+
+// ==============================================================================
+// EJERCICIO 4
+
+// Hacer un formulario para obtener una flor por su nombre.
+// Se mostrará el resultado en #ejercicio4
+
+
+// ==============================================================================
+// EJERCICIO 5
+
+// Crea y programa un formulario para añadir flores al array.
+// Por ejemplo:
+// flor: cyclamen, color:rosa, floracion: invierno, stock:true
+// Tiene que actualizarse automáticamente la lista del ejercicio 1
+
+
+// ============================================================================== 
+/* E X T R A S */
+
+// ==============================================================================
+// EJERCICIO 6
+
+// Crea y programa un formulario para añadir precios a las flores:
+// rosa roja : 8.00€
+// rosa blanca : 10.00€
+// jazmin: 12.00€
+// crisantemo: 5.00€
+// cerezo: 25.00€
+// cyclamen: 4.50€
+// Tiene que actualizarse automáticamente la lista del ejercicio 1
+
+// ==============================================================================
+// EJERCICIO 7
+
+// Crea la forma de eliminar elementos del array
+// Tiene que actualizarse automáticamente la lista del ejercicio 1
+
+// ==============================================================================
+// EJERCICIO 8
+
+// Crea la forma de editar elementos del array de flores
+// Todas las propiedades deben poder ser editadas: nombre, color, floración, stock  y precio
+// Tiene que actualizarse automáticamente la lista del ejercicio 1
