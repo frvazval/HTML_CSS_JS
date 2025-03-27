966b58ee9ef724cda471a9d245d1307cb3a56f48
Fvazquez <fvazquez2@hotmail.com>
2025-03-28 00:48:04 +0100

    casa

--- estacion_metereoligica/js/scripts.js
+++ estacion_metereoligica/js/scripts.js
@@ -1,8 +1,1 @@
-window.onload = function () {
-    // ...existing code...
-    const ciudadInput = document.getElementById("ciudad");
-    ciudadInput.value = "Barcelona"; // Set default city to Barcelona
-    document.getElementById("nombreCiudad").textContent = ciudadInput.value;
-    actualizarInformacion();
-    // ...existing code...
-};
+
