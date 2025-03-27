e514b222bd2bb9fbad911207139a2afade02debf
Fvazquez <fvazquez2@hotmail.com>
2025-03-27 22:55:51 +0100

    casa

--- estacion_metereoligica/js/scripts.js
+++ estacion_metereoligica/js/scripts.js
@@ -1,7 +1,13 @@
 function updateDateTime() {
     const now = new Date();
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
-    document.getElementById("date-time").textContent = "Fecha y Hora: " + now.toLocaleDateString("es-ES", options);
+    const dateTimeElement = document.getElementById("date-time");
+    dateTimeElement.innerHTML = "Fecha y Hora: <strong>" + now.toLocaleDateString("es-ES", options) + "</strong>";
+
+    // Move the date-time element between the language selector and city input
+    const idiomaSelector = document.getElementById("idioma");
+    const ciudadInput = document.getElementById("ciudad");
+    idiomaSelector.parentNode.insertBefore(dateTimeElement, ciudadInput);
 }
 window.onload = updateDateTime;
 setInterval(updateDateTime, 1000);
@@ -14,8 +20,7 @@ function actualizarInformacion() {
     console.log("Ciudad ingresada:", ciudad);
     const appid = "0cb0fdd74d71dd20b2b35ef7a5b0925c"
     const nombreCiudad = document.getElementById("nombreCiudad") 
-    const temperatura = document.getElementById("temperaruta")
-    
+    const temperatura = document.getElementById("temperatura")
     
     
     let url = `https://api.openweathermap.org/data/2.5/weather?appid=${appid}&units=metric&lang=${idioma}&q=${ciudad}`
@@ -24,14 +29,9 @@ function actualizarInformacion() {
     .then(datos => datos.json())
     .then(datos => {
         nombreCiudad.textContent = `${datos.name}`
+        temperatura.textContent = `${datos['main']['temp']}ºC`
         // divDatos.innerHTML += `<p>Temperatura actual : ${datos['main']['temp']}ºC</p>`
         // divDatos.innerHTML += `<p>Descripción : ${datos['weather'][0].description}</p>`
         // divDatos.innerHTML += `<p><img id="imagen" src="https://www.imelcf.gob.pa/wp-content/plugins/location-weather/assets/images/icons/weather-icons/${datos['weather'][0]['icon']}.svg"></p>`
     })
-
-   
-
-
-    
-    
 }
