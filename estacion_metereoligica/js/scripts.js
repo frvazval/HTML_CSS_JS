af6337be94b3ae68f92817cb4be9a0a6e6bfe90f
Fvazquez <foapgenerico@365.stucom.com>
2025-03-27 20:00:17 +0100

    stucom

--- estacion_metereoligica/js/scripts.js
+++ estacion_metereoligica/js/scripts.js
@@ -5,3 +5,32 @@ function updateDateTime() {
 }
 window.onload = updateDateTime;
 setInterval(updateDateTime, 1000);
+
+function actualizarInformacion() {
+
+    const idioma = document.getElementById("idioma").value;
+    const ciudad = document.getElementById("ciudad").value;
+    console.log("Idioma seleccionado:", idioma);
+    console.log("Ciudad ingresada:", ciudad);
+    const appid = "0cb0fdd74d71dd20b2b35ef7a5b0925c"
+    const nombreCiudad = document.getElementById("nombreCiudad") 
+    
+    
+    
+    let url = `https://api.openweathermap.org/data/2.5/weather?appid=${appid}&units=metric&lang=${idioma}&q=${ciudad}`
+
+    fetch(url)
+    .then(datos => datos.json())
+    .then(datos => {
+        nombreCiudad.textContent = `${datos.name}`
+        // divDatos.innerHTML += `<p>Temperatura actual : ${datos['main']['temp']}ºC</p>`
+        // divDatos.innerHTML += `<p>Descripción : ${datos['weather'][0].description}</p>`
+        // divDatos.innerHTML += `<p><img id="imagen" src="https://www.imelcf.gob.pa/wp-content/plugins/location-weather/assets/images/icons/weather-icons/${datos['weather'][0]['icon']}.svg"></p>`
+    })
+
+   
+
+
+    
+    
+}
