54d3393f0f1928fb35c6317bbfedd9a876f7312f
Fvazquez <foapgenerico@365.stucom.com>
2025-03-27 18:36:35 +0100

    stucom

--- App_METEO/js/app.js
+++ App_METEO/js/app.js
@@ -1,7 +1,7 @@
 // https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid=0cb0fdd74d71dd20b2b35ef7a5b0925c&units=metric&lang=es
 
 const appid = "0cb0fdd74d71dd20b2b35ef7a5b0925c"
-const ciudad = "Barcelona"
+const ciudad = "kiev"
 const idioma = "es"
 let url = `https://api.openweathermap.org/data/2.5/weather?appid=${appid}&units=metric&lang=${idioma}&q=${ciudad}`
 
