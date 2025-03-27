81c548929cad8ca678219f8cbd1aa9dc6787403b
Fvazquez <foapgenerico@365.stucom.com>
2025-03-27 18:25:41 +0100

    stucom

--- App_METEO/js/app.js
+++ App_METEO/js/app.js
@@ -1,1 +1,15 @@
-https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid=0cb0fdd74d71dd20b2b35ef7a5b0925c&units=metric&lang=es
+// https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid=0cb0fdd74d71dd20b2b35ef7a5b0925c&units=metric&lang=es
+
+let url = "https://api.openweathermap.org/data/2.5/weather?appid=0cb0fdd74d71dd20b2b35ef7a5b0925c&units=metric&lang=es&q="
+url += "Barcelona"
+
+const divDatos = document.getElementById("divDatos")
+fetch(url)
+.then(datos => datos.json())
+.then(datos => {
+    divDatos.innerHTML = `<p>Ciudad : ${datos.name}</p>`
+    divDatos.innerHTML += `<p>Temperatura actual : ${datos['main']['temp']}ºC</p>`
+    divDatos.innerHTML += `<p>Descripción : ${datos['weather'][0].description}</p>`
+    divDatos.innerHTML += `<p><img id="imagen" src="https://www.imelcf.gob.pa/wp-content/plugins/location-weather/assets/images/icons/weather-icons/${datos['weather'][0]['icon']}.svg"></p>`
+})
+
