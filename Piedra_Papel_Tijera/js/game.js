3d7c90e1ecc866d0362d3d2df31c3886d07b71bc
Fvazquez <foapgenerico@365.stucom.com>
2025-04-08 17:47:27 +0200

    stucom

--- Piedra_Papel_Tijera/js/game.js
+++ Piedra_Papel_Tijera/js/game.js
@@ -1,6 +1,7 @@
 // Nombre del usuario
 let buttonNombre = document.querySelector('header button');
 let pNombre = document.querySelector('header p')
+let respuesta = document.getElementById('respuesta')
 
 buttonNombre.addEventListener('click', () => {
     let nombreUsuario = prompt("¿Cuál es tu nombre?")
@@ -14,14 +15,24 @@ formJuego.addEventListener('submit', (e) => {
     e.preventDefault(); // Evitar el envío del formulario
     let jugadaHumano = formJuego['jugada'].value; // Obtener la jugada del usuario
     
+    
     // Jugada del PC
     let numMinimo = 1
     let numMaximo = 3
     let JugadaPC = Math.floor(Math.random() * (numMaximo - numMinimo + 1)) + numMinimo; // Generar un número aleatorio entre 1 y 3
-
+    
+    let mensaje = ""
     if (jugadaHumano == JugadaPC) {
+        mensaje = "¡ Empate !"
+    } else if ((jugadaHumano == 1 && JugadaPC == 3) || (jugadaHumano == 2 && JugadaPC == 1) || (jugadaHumano == 3 && JugadaPC == 2)) {
+        mensaje = "¡ Has ganado !"
 
+    } else {
+        mensaje = "¡ Has perdido !"
     }
+
+    respuesta.innerHTML = `<p>${mensaje}</p>`
+
 })
 
 
